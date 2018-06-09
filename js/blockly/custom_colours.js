91beb1d7f6e2a418ea7ec1afbcb4d1bf9e506234
adaws97 <alexander_dawson@ymail.com>
2018-06-09 09:50:18 +0100

    Add custom colours file blockl

---
+++ js/blockly/custom_colours.js
@@ -0,0 +1,12 @@
+/**
+* Redefine the colours of the predefined block categories
+*/
+'use strict';
+
+goog.provide('Blockly.Msg.customColours');
+
+/** @export */ Blockly.Msg.LOOPS_HUE = 35;
+/** @export */ Blockly.Msg.LOGIC_HUE = 140;
+/** @export */ Blockly.Msg.VAR_NUMBER_HUE = 5;
+/** @export */ Blockly.Msg.VAR_STRING_HUE = 90;
+/** @export */ Blockly.Msg.VAR_BOOL_HUE = 180;
